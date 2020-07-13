20850d26287b46d78e2a8d5ddd2f00ea2faaa76b
MostafaYahia <mostafayehyax23@gmail.com>
2020-07-13 13:04:15 +0200

    Init commit

---
+++ src/main/java/com/mostafayehya/deliciousrecipes/DeliciousRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.mostafayehya.deliciousrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciousRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliciousRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mostafayehya/deliciousrecipes/DeliciousRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafayehya.deliciousrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
